4f34d2932ea4b035091d0e13930c3cb75295e6e7
Dmytro Kaplin <dkaplinwdevs@gmail.com>
2025-05-21 09:33:45 +0200

    added default attribute group name

---
+++ Model/ConfigProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EPuzzle\Base\Model;
+
+/**
+ * Used to provide the base config for all modules
+ */
+class ConfigProvider
+{
+    public const string PRODUCT_ATTRIBUTE_GROUP = 'ePuzzle';
+}
